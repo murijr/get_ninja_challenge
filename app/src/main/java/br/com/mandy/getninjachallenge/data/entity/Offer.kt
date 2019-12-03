e69a2a54a65e60bc86f2424ad8c1e9761f58d397
Amauri <ardsjj@gmail.com>
2019-12-03 01:37:00 -0300

    change names

--- app/src/main/java/br/com/mandy/getninjachallenge/data/entity/Offer.kt
+++ app/src/main/java/br/com/mandy/getninjachallenge/data/entity/Offer.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
-class Offer: Serializable {
+class Offer {
     @SerializedName("state")
     @Expose
     var state: String? = null
