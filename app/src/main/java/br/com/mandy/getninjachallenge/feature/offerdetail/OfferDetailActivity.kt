b05d1b9e5956b8b1b52211aebfcb3ba307e50588
Amauri <ardsjj@gmail.com>
2019-12-05 23:18:55 -0300

    change names

--- app/src/main/java/br/com/mandy/getninjachallenge/feature/offerdetail/OfferDetailActivity.kt
+++ app/src/main/java/br/com/mandy/getninjachallenge/feature/offerdetail/OfferDetailActivity.kt
@@ -1,6 +1,5 @@
 package br.com.mandy.getninjachallenge.feature.offerdetail
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
