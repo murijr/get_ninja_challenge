266ea1e38aecacb80e6615db5cf5de7de954bd4f
Amauri <ardsjj@gmail.com>
2019-12-06 05:51:08 -0300

    change names

--- app/src/main/java/br/com/mandy/getninjachallenge/data/repository/lead/LeadRemoteRepository.kt
+++ app/src/main/java/br/com/mandy/getninjachallenge/data/repository/lead/LeadRemoteRepository.kt
@@ -62,7 +62,10 @@ class LeadRemoteRepository(private val converter: GenericConverter):
     ) {
         thread {
             val bitmap = Picasso.get()
-                .load("https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyB9Orb1AUnAmhxWSCbaODmM7ONI1tl7BoM")
+                .load("https://maps.googleapis.com/maps/api/staticmap?" +
+                        "size=600x300&markers=color:red%7Clabel:C%7C" +
+                        "${geolocation?.latitude},${geolocation?.longitude}&" +
+                        "key=AIzaSyB9Orb1AUnAmhxWSCbaODmM7ONI1tl7BoM")
                 .get()
             onSuccess?.invoke(bitmap)
         }
